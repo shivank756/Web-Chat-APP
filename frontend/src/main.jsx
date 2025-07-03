e65a6ceb8889f1541b36ed121835bad88b88b865
shivank756 <133559836+shivank756@users.noreply.github.com>
2025-07-03 15:53:14 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import  store  from './Redux/store.js'
 
 import { Provider } from 'react-redux'
 // axios.defaults.baseURL = 'http://192.168.1.9:3000'  // use your PC IP here
-// axios.defaults.withCredentials = true 
+axios.defaults.withCredentials = true 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
