3b37fec8cb8601155891e32a1dc7033096dbdc25
shivank756 <133559836+shivank756@users.noreply.github.com>
2025-07-03 16:10:10 +0530

    Update socket.js

--- Backend/Socket/socket.js
+++ Backend/Socket/socket.js
@@ -1,55 +1,5 @@
 
 
-// import { Server } from "socket.io";
-// import http from "http";
-// import express from "express";
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: ['http://localhost:5173'], // or add deployed client URL too
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// const usersocketMap = {}; // userId -> socketId
-
-// export const getReceiverSocketId = (receiverObjectId) => {
-//   return usersocketMap[receiverObjectId];
-// };
-
-// io.on('connection', (socket) => {
-//   console.log('🔌 user connected', socket.id);
-
-//   const userId = socket.handshake.query.userId;
-//   if (userId) {
-//     usersocketMap[userId] = socket.id;
-//   }
-
-//   // Send updated online users to everyone
-//   io.emit('getOnlineUsers', Object.keys(usersocketMap));
-
-//   // ✅ Listen for new messages inside the connection
-//   socket.on('newMessage', (message) => {
-//     const receiverSocketId = usersocketMap[message.to];
-//     if (receiverSocketId) {
-//       io.to(receiverSocketId).emit('receiveMessage', message);
-//     }
-//   });
-
-//   // Cleanup on disconnect
-//   socket.on('disconnect', () => {
-//     console.log('🔌 user disconnected', socket.id);
-//     if (userId) {
-//       delete usersocketMap[userId];
-//     }
-//     io.emit('getOnlineUsers', Object.keys(usersocketMap));
-//   });
-// });
-
-// export { app, io, server };
-
 
 import { Server } from "socket.io";
 import http from "http";
@@ -60,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173'],
+    origin: ['https://web-chat-app-frontend-zu95.onrender.com'],
     methods: ["GET", "POST"],
   },
 });
