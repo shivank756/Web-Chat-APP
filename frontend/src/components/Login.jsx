ac8392a223ad37f96d27c3e9e2c7a1d0476aef6f
shivank756 <133559836+shivank756@users.noreply.github.com>
2025-07-03 16:33:44 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
         const { username, password } = user;
         try {
             const res = await axios.post(
-                'https://web-chat-app-u7yl.onrender.com',
+                'https://web-chat-app-u7yl.onrender.com/api/v1/user/login',
                 {
                     UserName: username,
                     Password: password,
